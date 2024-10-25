829358c06003f62a7f6908610d53a7757155a33b
Samuel de Sá <samueldsa03032005@gmail.com>
2024-10-25 19:11:57 -0300

    Update RoboEntrega.ino

--- Arduino/RoboEntrega.ino
+++ Arduino/RoboEntrega.ino
@@ -1,47 +1,38 @@
-#include <Adafruit_TCS34725softi2c.h>
+#include <Adafruit_TCS34725.h>
 
-#include "Wire.h"
-#include "Adafruit_TCS34725.h"
+// #include <Adafruit_TCS34725softi2c.h>
+int ledS = 6;
+int lumin = 100;
+int segueE = 7;
+int segueD = 8;
+int porta = 9;
+int velE = 2;
+int velD = 3;
+int motoD1 = 22;
+int motoD2 = 24;
+int motoE1 = 26;
+int motoE2 = 28;
 
-#define SDApinD A4
-#define SCLpinD A5
-
-#define SDApinE 5
-#define SCLpinE 4
-
-#define SDApinC 21
-#define SCLpinC 20
-
-// Configuração do sensor de cores
-/* Initialise with specific int time and gain values and custom SDA / SCL pin */
-Adafruit_TCS34725softi2c tcsD = Adafruit_TCS34725softi2c(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X, SDApinD, SCLpinD);
-Adafruit_TCS34725softi2c tcsE = Adafruit_TCS34725softi2c(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X, SDApinE, SCLpinE);
-Adafruit_TCS34725softi2c tcsC = Adafruit_TCS34725softi2c(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X, SDApinC, SCLpinC);
-
-
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_600MS, TCS34725_GAIN_1X);
+// Adafruit_TCS34725softi2c tcsC = Adafruit_TCS34725softi2c(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X, 13, 12);
+// Adafruit_TCS34725softi2c tcsE = Adafruit_TCS34725softi2c(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X, 5, 4);
+// Adafruit_TCS34725softi2c tcsD = Adafruit_TCS34725softi2c(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X, 7, 6);
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-
-  if (tcs.begin()) {
-    Serial.println("Sensor encontrado");
-  } else {
-    Serial.println("Sensor não encontrado, cheque suas conexões.");
-    while (1);
-  }
-  analogWrite(7, 200);
+  pinMode(ledS, OUTPUT);
+  analogWrite(ledS, lumin);
+  tcs.begin();
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   uint16_t r, g, b, c;
-
-  // Obtém os dados brutos de vermelho, verde, azul e a claridade
   tcs.getRawData(&r, &g, &b, &c);
-
-  // Envia os valores de cor RGB para o serial no formato "R,G,B"
-  Serial.print(r); Serial.print(",");
-  Serial.print(g); Serial.print(",");
+  Serial.print(r);
+  Serial.print(",");
+  Serial.print(g);
+  Serial.print(",");
   Serial.println(b);
-
-  delay(50);  // Atraso para diminuir a frequência das leituras
 }
